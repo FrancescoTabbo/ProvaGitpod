b937c8014567b0ca3630d514d6eb26011034f558
Francesco Tabbò <francesco.tabbo2000@gmail.com>
2019-05-06 11:56:47 +0000

    inizio progetto finale

---
+++ Mono-rent/src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  data: Object;
+   o :Observable<Object>;
+   constructor(public http: HttpClient) {}
+   Login(): void {
+   this.http
+     .post('',
+       JSON.stringify({
+         body: 'bar',
+         title: 'foo',
+         userId: 1
+       })
+     )
+     .subscribe(data => {
+       this.data = data;
+
+     });
+      }
+    Registrati(): void {
+   this.http
+     .post('',
+       JSON.stringify({
+         body: 'bar',
+         title: 'foo',
+         userId: 1
+       })
+     )
+     .subscribe(data => {
+       this.data = data;
+
+     });
+      }
+
+}
