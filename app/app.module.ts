e7f2abed84299ffed5f09850846726274b701d02
Francesco Tabbò <47140271+FrancescoTabbo@users.noreply.github.com>
2019-05-08 22:58:47 +0200

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'; //Importa Il modulo http
+
+import {
+FormsModule,
+  ReactiveFormsModule
+ } from '@angular/forms';
+import { SongListComponent } from './song-list/song-list.component';
+import { DettagliAlbumComponent } from './dettagli-album/dettagli-album.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SongListComponent,
+    DettagliAlbumComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
